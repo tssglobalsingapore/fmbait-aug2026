67e3c244690651dd17d2033c6c806b4c6d554500
tssglobalsingapore <166129848+tssglobalsingapore@users.noreply.github.com>
2026-08-22 14:00:22 +0800

    Update app.py

--- app.py
+++ app.py
@@ -86,7 +86,9 @@ demo = gr.Interface(
 )
 
 if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 7860))
+
     demo.launch(
         server_name="0.0.0.0",
-        server_port=7860
+        server_port=port
     )
